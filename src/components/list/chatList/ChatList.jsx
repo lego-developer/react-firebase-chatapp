8a8665602aa0cfaae15ee4cdefd93489340367e8
lego_developer <legos1181@gmail.com>
2024-06-22 15:16:41 +0530

    login , logout , search user

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,10 +1,45 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./chatList.css"
 import AddUser from './addUser/AddUser'
+import { doc, onSnapshot, getDoc } from "firebase/firestore";
+import { useUserStore } from "../../../lib/userStore"
+import { db } from '../../../lib/firebase';
+
 
 const ChatList = () => {
 
     const [addMode, setAddMode] = useState(false)
+    const [chats, setChats] = useState([])
+
+    const { currentUser } = useUserStore()
+
+    useEffect(() => {
+
+        const unSub = onSnapshot(doc(db, "userchats", currentUser.id), async (res) => {
+
+            const items = res.data().chats
+            const promises = items.map(async (item) => {
+
+                const userDocRef = doc(db, "users", item.receiverId);
+                const userDocSnap = await getDoc(userDocRef);
+                const user = userDocSnap.data()
+                return { ...item, user }
+
+            })
+            const chatData = await Promise.all(promises)
+            setChats(chatData.sort((a, b) => b.updatedAt - a.updatedAt))
+
+        });
+
+        return () => {
+            unSub()
+        }
+
+    }, [currentUser.id])
+
+    // console.log(chats)
+
+
 
     return (
         <div className='chatList' >
@@ -15,49 +50,20 @@ const ChatList = () => {
                 </div>
                 <img src={addMode ? "/minus.png" : "/plus.png"} alt="" className='add' onClick={() => setAddMode(!addMode)} />
             </div>
-            <div className="item">
-                <img src="./avatar.png" alt="" />
-                <div className="texts">
-                    <span>Jane Doe</span>
-                    <p>Hello</p>
+            {chats.map(chat => (
+
+                <div className="item" key={chat.chatId} >
+                    <img src="./avatar.png" alt="" />
+                    <div className="texts">
+                        <span>Jane Doe</span>
+                        <p>{chat.lastMessage}</p>
+                    </div>
                 </div>
-            </div>
-            <div className="item">
-                <img src="./avatar.png" alt="" />
-                <div className="texts">
-                    <span>Jane Doe</span>
-                    <p>Hello</p>
-                </div>
-            </div>
-            <div className="item">
-                <img src="./avatar.png" alt="" />
-                <div className="texts">
-                    <span>Jane Doe</span>
-                    <p>Hello</p>
-                </div>
-            </div>
-            <div className="item">
-                <img src="./avatar.png" alt="" />
-                <div className="texts">
-                    <span>Jane Doe</span>
-                    <p>Hello</p>
-                </div>
-            </div>
-            <div className="item">
-                <img src="./avatar.png" alt="" />
-                <div className="texts">
-                    <span>Jane Doe</span>
-                    <p>Hello</p>
-                </div>
-            </div>
-            <div className="item">
-                <img src="./avatar.png" alt="" />
-                <div className="texts">
-                    <span>Jane Doe</span>
-                    <p>Hello</p>
-                </div>
-            </div>  
-           { addMode && <AddUser/>}
+                
+            ))}
+
+
+            {addMode && <AddUser />}
         </div>
     )
 }
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import "./login.css"
 import { toast } from "react-toastify"
 
-import { createUserWithEmailAndPassword } from "firebase/auth"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"
 import { auth, db } from "../../lib/firebase"
 import { doc, setDoc } from "firebase/firestore";
 import upload from "../../lib/upload"
@@ -13,6 +13,8 @@ const Login = () => {
         file: null,
         url: ""
     })
+
+    const [loading, setLoading] = useState(false)
 
     const handleAvatar = (e) => {
         if (e.target.files[0]) {
@@ -27,6 +29,8 @@ const Login = () => {
 
     const handleRegister = async (e) => {
         e.preventDefault()
+
+        setLoading(true)
 
         const formData = new FormData(e.target)
         const { username, email, password } = Object.fromEntries(formData)
@@ -53,13 +57,28 @@ const Login = () => {
         } catch (error) {
             console.log(error)
             toast.error(error.message)
+        } finally {
+            setLoading(false)
         }
 
     }
 
-    const handleLogin = e => {
+    const handleLogin = async (e) => {
         e.preventDefault()
-        toast.error("Hello")
+        setLoading(true)
+
+        const formData = new FormData(e.target)
+        const { email, password } = Object.fromEntries(formData)
+
+        try {
+
+            await signInWithEmailAndPassword(auth,email,password)
+
+        } catch (error) {
+            toast.error(error.message)
+        } finally{
+            setLoading(false)
+        }
     }
 
 
@@ -71,7 +90,7 @@ const Login = () => {
                 <form action="" onSubmit={handleLogin} >
                     <input type="text" placeholder="Email" name="email" />
                     <input type="password" placeholder="Password" name="password" />
-                    <button>Sign In</button>
+                    <button disabled={loading} >{loading ? "Loading..." : "Sign In"}</button>
                 </form>
             </div>
             <div className="separator"></div>
@@ -85,7 +104,7 @@ const Login = () => {
                     <input type="text" placeholder="Username" name="username" />
                     <input type="text" placeholder="Email" name="email" />
                     <input type="password" placeholder="Password" name="password" />
-                    <button>Sign Up</button>
+                    <button disabled={loading} >{loading ? "Loading..." : "Sign Up"}</button>
                 </form>
             </div>
         </div>
